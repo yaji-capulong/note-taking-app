9a920d3afc542080c36dec59221cc2eef963eda7
Yaj Capulong <maryjay.capulong@gmail.com>
2024-08-02 14:38:03 -0700

    Add Admin and Log in system

---
+++ AdminSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteTakingApp
+{
+    public class AdminSystem : IUserLibrary
+    {
+        private List<User> users = new List<User>();
+        public void AdminMainPage()
+        {
+            Console.WriteLine("Welcome Administrator. Choose an option:");
+            Console.WriteLine("A. Add new user" + 
+                                "\nB. View Users" + 
+                                "\nC. Delete user");
+        }
+        public void AddNewUser()
+        {
+
+        }
+        public void ViewUsers()
+        {
+
+        }
+        public void DeleteUser()
+        {
+
+        }
+    }
+}
---
+++ IUserLibrary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteTakingApp
+{
+    public interface IUserLibrary
+    {
+        void AdminMainPage();
+        void AddNewUser();
+        void DeleteUser();
+        void ViewUsers();
+    }
+}
